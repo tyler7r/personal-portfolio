1fe98bd9880f2716b059d72e0d75c10c7e0b155e
Tyler Randall <tyler7r@gmail.com>
2023-08-18 13:50:13 -0400

    Backbone of header and projects containers

--- src/Header.js
+++ src/Header.js
@@ -2,5 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Header = (props) => {
-    
+    return (
+        <div className='header-section'>
+            <div className='header-image'>Tyler Randall</div>
+            <div className='header-info'>
+                <h2 className='header-title'>About me</h2>
+                <p className='header-description'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+                <div className='header-buttons'>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 export const Projects = (props) => {
-    
+    return (
+        <div className='project-section'>
+            <div className="project-cards">
+                <div className='project-card' id='main-project'>
+                    <div className='project-screenshot'>Screenshot</div>
+                    <div className="project-info">
+                        <div className='project-name'>The Odinbook</div>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+                        <div className='project-description'></div>
+                    </div>
+                </div>
+                <div className='project-card'>
+                    <div className='project-screenshot'>Screenshot</div>
+                    <div className="project-info">
+                        <div className='project-name'>Shopping Cart</div>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+                        <div className='project-description'></div>
+                    </div>
+                </div>
+                <div className='project-card'>
+                    <div className='project-screenshot'>Screenshot</div>
+                    <div className="project-info">
+                        <div className='project-name'>Memory Card</div>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+                        <div className='project-description'></div>
+                    </div>
+                </div>
+                <div className='project-card'>
+                    <div className='project-screenshot'>Screenshot</div>
+                    <div className="project-info">
+                        <div className='project-name'>Knight Travails</div>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+                        <div className='project-description'></div>
+                    </div>
+                </div>
+                <div className='project-card'>
+                    <div className='project-screenshot'>Screenshot</div>
+                    <div className="project-info">
+                        <div className='project-name'>Todo List / Calculator</div>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+                        <div className='project-description'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
