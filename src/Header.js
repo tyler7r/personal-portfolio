745fbbd8d26684332eeee8f4483d8594fdf2b8f1
Tyler Randall <tyler7r@gmail.com>
2023-08-18 14:28:42 -0400

    Header layout for mobile

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './styles/header.css'
 
 export const Header = (props) => {
     return (
@@ -11,9 +12,9 @@ export const Header = (props) => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 </p>
-                <div className='header-buttons'>
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+                <div className='header-buttons-container'>
+                    <img className='header-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
+                    <img className='header-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
                 </div>
             </div>
         </div>
