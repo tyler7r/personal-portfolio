4c23d2a18fba0ab9ed7dbc016f39ee996332564b
Tyler Randall <tyler7r@gmail.com>
2023-08-23 13:48:05 -0400

    Style header for mobile and laptop sizes

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Headshot from './images/trandall_profile.jpg'
 import './styles/header.css'
 
 export const Header = (props) => {
     return (
         <div className='header-section'>
-            <div className='header-image'>Tyler Randall</div>
+            <div className='header-image-container'>
+                <img src={Headshot} alt='headshot' className='header-image' />
+                <div className='header-name'>Tyler Randall</div>
+            </div>
+            <div className='header-buttons-container'>
+                <Link to='https://github.com/tyler7r'><img className='header-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' /></Link>
+                <img className='header-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
+            </div>
             <div className='header-info'>
                 <h2 className='header-title'>About me</h2>
                 <p className='header-description'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 </p>
-                <div className='header-buttons-container'>
-                    <Link to='https://github.com/tyler7r'><img className='header-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' /></Link>
-                    <img className='header-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" alt='linkdin-icon' />
-                </div>
             </div>
+
         </div>
     )
 }
--- src/Projects.js
+++ src/Projects.js
@@ -5,6 +5,8 @@ import OdinbookImage from './images/odinbook-screenshot.jpg'
 import ShoppingCartImage from './images/shopping-cart.jpg'
 import KnightTravailsImage from './images/knight-travails.jpg'
 import CalculatorImage from './images/calculator.jpg'
+import MemoryCardImage from './images/memory-card.jpg'
+import WaldoImage from './images/waldo.jpg'
 import './styles/projects.css'
 
 
@@ -21,6 +23,17 @@ export const Projects = (props) => {
                             <Link to='https://github.com/tyler7r/TOP-odin-book'><img className='project-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' /></Link>
                             <Link to='https://top-odinbook.netlify.app/odinbook/login'><img className='project-btn' src={OpenProjectIcon} alt='open-project' /></Link>
                         </div>
+                        <p className='project-description'>This is the description</p>
+                    </div>
+                </div>
+                <div className='project-card'>
+                    <div className='project-preview-container'><img src={WaldoImage} className='project-preview' alt='waldo-game-preview' /></div>
+                    <div className="project-info">
+                        <div className='project-name'>Where's Waldo Game</div>
+                        <div className="project-btns-container">
+                            <Link to='https://github.com/tyler7r/TOP-photo-tagging'><img className='project-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' /></Link>
+                            <Link to='https://top-photo-tagging.netlify.app/'><img className='project-btn' src={OpenProjectIcon} alt='open-project' /></Link>
+                        </div>
                         <div className='project-description'>This is the description</div>
                     </div>
                 </div>
@@ -36,12 +49,12 @@ export const Projects = (props) => {
                     </div>
                 </div>
                 <div className='project-card'>
-                    <div className='project-preview'>Screenshot</div>
+                    <div className='project-preview-container'><img src={MemoryCardImage} className='project-preview' alt='memory-card-preview' /></div>
                     <div className="project-info">
-                        <div className='project-name'>Memory Card</div>
+                        <div className='project-name'>Survivor Memory</div>
                         <div className="project-btns-container">
-                            <img className='project-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' />
-                            <img className='project-btn' src={OpenProjectIcon} alt='open-project' />
+                            <Link to='https://github.com/tyler7r/TOP-memory-card'><img className='project-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' /></Link>
+                            <Link to='https://top-memory-card.netlify.app/'><img className='project-btn' src={OpenProjectIcon} alt='open-project' /></Link>
                         </div>
                         <div className='project-description'>This is the description</div>
                     </div>
@@ -60,7 +73,7 @@ export const Projects = (props) => {
                 <div className='project-card'>
                     <div className='project-preview-container'><img src={CalculatorImage} alt='calculator-preview' className='project-preview' /></div>
                     <div className="project-info">
-                        <div className='project-name'>Todo List / Calculator</div>
+                        <div className='project-name'>Calculator</div>
                         <div className="project-btns-container">
                             <Link to='https://github.com/tyler7r/TOP-calculator'><img className='project-btn' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github-icon' /></Link>
                             <Link to='https://tyler7r.github.io/TOP-calculator/'><img className='project-btn' src={OpenProjectIcon} alt='open-project' /></Link>
